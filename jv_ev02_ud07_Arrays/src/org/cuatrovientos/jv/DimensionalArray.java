28a231454474bbe986d73d81e8192366c5a5d3c6
Salvador Arance <salvaarancegallego@gmail.com>
2025-12-10 15:43:40 +0100

    Cambiar Integers por Floats en DimensionalArray

--- jv_ev02_ud07_Arrays/src/org/cuatrovientos/jv/DimensionalArray.java
+++ jv_ev02_ud07_Arrays/src/org/cuatrovientos/jv/DimensionalArray.java
@@ -7,19 +7,19 @@ public class DimensionalArray {
 	public static void main(String[] args) {
 		Scanner escaner = new Scanner(System.in);
 		String entrada = "";
-		int arrayEnteros[][] = new int[5][5];
-		int sumas[] = new int [5];
-		int medias[] = new int [5];
+		float arrayFloats[][] = new float[5][5];
+		float sumas[] = new float [5];
+		float medias[] = new float [5];
 		boolean esNumero = false;
 
-		for (int i = 0; i < arrayEnteros.length; i++) {
-			for (int j = 0; j < arrayEnteros[i].length; j++) {
+		for (int i = 0; i < arrayFloats.length; i++) {
+			for (int j = 0; j < arrayFloats[i].length; j++) {
 				do {
 					esNumero = true;
 					System.out.print("Número [" + i + "] [" + j + "]: ");
 					entrada = escaner.nextLine();
 					try {
-						arrayEnteros[i][j] = Integer.parseInt(entrada);
+						arrayFloats[i][j] = Float.parseFloat(entrada);
 					} catch (NumberFormatException e) {
 						esNumero = false;
 					}
@@ -30,16 +30,16 @@ public class DimensionalArray {
 			}
 		}
 
-		for (int i = 0; i < arrayEnteros.length; i++) {
-			for (int j = 0; j < arrayEnteros[i].length; j++) {
-				sumas[i] += arrayEnteros[i][j];
+		for (int i = 0; i < arrayFloats.length; i++) {
+			for (int j = 0; j < arrayFloats[i].length; j++) {
+				sumas[i] += arrayFloats[i][j];
 			}
 			
-			medias[i] = sumas[i] / arrayEnteros.length;
+			medias[i] = sumas[i] / arrayFloats.length;
 			
 		}
 		
-		for (int i =0; i < arrayEnteros.length; i++) {
+		for (int i =0; i < arrayFloats.length; i++) {
 			System.out.println("La media de la fila " + i + " es: " + medias[i]);
 		}
 		escaner.close();
