b16b9bd074fd070da8d7f511c0cf094410a4c470
salva <salvaarancegallego@gmail.com>
2026-01-08 09:18:08 +0100

    Teoría JAVA CLASES

--- jv_ev02_ud08_Clases/src/org/cuatrovientos/jv/GestorPersonas.java
+++ jv_ev02_ud08_Clases/src/org/cuatrovientos/jv/GestorPersonas.java
@@ -6,7 +6,5 @@ public class GestorPersonas {
 		// TODO Auto-generated method stub
 		Persona p1 = new Persona("Juan", "Pérez", "26523239F", "calle muelle");
 		Persona p2 = new Persona("Pedrete", "Carlos", "26523339F", "calle gloosito");
-		System.out.println(p1.nombre);
-		System.out.println(p2.nombre);
 	}
 }
--- jv_ev02_ud08_Clases/src/org/cuatrovientos/jv/Persona.java
+++ jv_ev02_ud08_Clases/src/org/cuatrovientos/jv/Persona.java
@@ -14,4 +14,36 @@ public class Persona {
 		this.dirección = dirección;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getDirección() {
+		return dirección;
+	}
+
+	public void setDirección(String dirección) {
+		this.dirección = dirección;
+	}
+	
 }
