8f5ae11d7662b9e6805eabc3299ce49d9f59cb14
salva <salvaarancegallego@gmail.com>
2026-01-15 09:25:04 +0100

    Final de clase 15/01/26

--- jv_ev02_ud08_Clases/src/org/cuatrovientos/jv/CoinFlipper.java
+++ jv_ev02_ud08_Clases/src/org/cuatrovientos/jv/CoinFlipper.java
@@ -7,15 +7,19 @@ public class CoinFlipper {
 	public static void main(String[] args) {
 		Coin euro = new Coin();
 
+		
 		System.out.println(euro.getEstado());
+		
+		for (int j = 0; j < 19; j ++) {
+			for (int i = 0; i < 5; i++) {
+				euro.flip2();
+			}
 
-		for (int i = 0; i < 5; i++) {
-			euro.flip2();
+			System.out.println(euro.getEstado());
+
+			System.out.println(euro.flip());
 		}
-
-		System.out.println(euro.getEstado());
-
-		System.out.println(euro.flip());
+		
 	}
 
 }
