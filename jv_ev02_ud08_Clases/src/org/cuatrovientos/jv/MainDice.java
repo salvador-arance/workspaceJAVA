2fa37b21d3fd37ef22f352356d2d4c46a7ef9b9d
salva <salvaarancegallego@gmail.com>
2026-01-22 09:35:58 +0100

    Terminar ejercicio 5 clases

--- jv_ev02_ud08_Clases/src/org/cuatrovientos/jv/MainDice.java
+++ jv_ev02_ud08_Clases/src/org/cuatrovientos/jv/MainDice.java
@@ -6,11 +6,26 @@ public class MainDice {
 
 	public static void main(String[] args) {
 		Dice dadoNormal = new Dice();
+		int dadoNormalResultados[] = new int[100];
 		Dice dadoDiezCaras = new Dice(10);
+		int dadoDiezCarasResultados[] = new int[100];
 		Dice dadoVeinteCaras = new Dice(20, true);
+		int dadoVeinteCarasResultados[] = new int[100];
 		
+		for (int i = 0; i < 100; i++) {
+			dadoNormalResultados[i] = dadoNormal.roll();
+		}
 		
-		System.out.println("Resultado: " + dadoNormal.roll());
+		for (int i = 0; i < 100; i++) {
+			dadoDiezCarasResultados[i] = dadoDiezCaras.roll();
+		}
+		
+		for (int i = 0; i < 100; i++) {
+			dadoVeinteCarasResultados[i] = dadoVeinteCaras.roll();
+		}
+		
+		System.out.println(dadoNormalResultados[0] + " " + dadoDiezCarasResultados[0] + " " + dadoVeinteCarasResultados[0]);
+		
 	}
 
 }
